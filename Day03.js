38ceeb4de09cae1a2a22e5aef29030f5caed2340
CGuilhem <93581955+CGuilhem@users.noreply.github.com>
2022-12-07 18:53:34 +0100

    First commit

---
+++ Day03.js
@@ -0,0 +1,372 @@
+const input =
+`wgqJtbJMqZVTwWPZZT
+LHcTGHQhzrTzBsZFPHFZWFFs
+RnLRClzGzRGLGLGCNRjTMjJfgmffSffMqNgp
+WPLgsfLmLgqZvZgSRR
+RbwHdbDdQFFFMvvMjbhqhZZS
+lzTdldBDszfGcRsr
+ZjnhJjMjnbdnbHdFLmmfFLmnCCWFFl
+PpNwtRsNsZSsRwCfzQQBfQszCBsC
+PpwcqqVZRtbggggjcgJJ
+ntczBcVcgnHzgBHnVntcBBFhgsmmmssqWNWNWqLvNhsqTN
+bSSGdSDZbGSGdDmLmGTvTGmLFFhm
+PlFbDpJDPbPdPbZQZDZlSCDBfMVRwBzBtBQzfzRHVMVRtH
+fDVrmmrvcmCcVpfcfGlswpPwsttMpjJMPPjjtP
+RgSTdndFLbJqqPssWWjPWjPjHS
+FqgzQnTqJRRQqLLhTCDhDCDmcmlvvlhcVm
+nnqVtHbfVHZVmtlvmHtZtrFSFTRRFhRccTbrLsLSGr
+cCNJQJPJQgjjMQdDrGGsRhTFGFRFSpMS
+gzdCwWdjNPgzcJgjwdZtVlHHmvvmZlvffHnz
+FFgsgwNwWvggQsMWDwvQQvQcccdcJZDtJGBtVGGGtcVlzt
+rjfTrbjpjRSRTbTpzldjjHBtJGBdltJG
+RbrPTfpCfmbpmnfRRCvMvQWNBwFFgMsgBL
+zzLHgjjjdFHWbGBjjzcbgQRmSvqsSpmRsRSQSmRMWv
+ZfJVrwPhZhZlhQQqBSQSNSqM
+tnCfrDCltfPzHFFLBgngHz
+DCpwrrMhwCrCMVCpGFqpVDnWWTWBtnTWvWfvbbTdFWRv
+lmhhcsQPmTtTnnPBTB
+QmcjNJsJzHNljZsNqDCGGhwqCqhgDDZV
+hLfRnSLfhcndCCPfJJjzJfzt
+pHNWwDpGGNJBZjjNNj
+gmgwwHpWTpmGDmDTggqHmmDSsnnhvcqScdVVSVcjLrRcnq
+CdlTJgnQJVCllNVWTPZBmPPGhGRmghPRGs
+wHDrSwtHbmhRvHVZ
+DzzwrtVFjLNnMTCTLCWW
+SppdsnGpNVnZZZLPMlMPGq
+mdcfvTTbBddLJgZJLlcFqJ
+fzTvfwjjfzzCbvvjvQjWvNHVNNVWrRtdnVNppNprDp
+lmlCGTmNbZlbSFlbNGfnzWfWzCdWWfVdwRCf
+jDHtHHvLjQtqrsqpjBBLprRzwfwJfzzhRnczhWQVwzVR
+rqpPtpPjDqpqDLtLrPGGFSFgPlNZZSGMPnMZ
+pSHShqgSMzVpphFnJMFMBtssdjRJ
+PZDbZfmCDgDfDNQPwCflCQNJsjRBRBFsdBWBsJRjsbsFJF
+DQvCwwZrPPlffDmQCDwZQPmPhqVTcGhSHSLTpSGhLHzpGghr
+gPqgqqmmmPgsqvGmsMCCnfZZfvBpWZhVrrZdHBvH
+TSlcttTjRTDlDDTRhZVdrHHpWVnfVrtd
+SFJzFzcJjcRJwGGqJMMCwW
+gzWNLSjRLzlNqqQMLhvQccGGmcQm
+fFrttPdTFTrpVwGpbdVQQp
+HnTBGfHTGzWWqCqngn
+SddrLdVpjjVSgRBszFswzwlV
+mtPMbMqPMvqHHHDTTglBvsFshFFg
+bmBHbtPctMtbMNMtbPtPqHmMjpdZdcJGrjSWWZGjJZnjndWj
+nljWJHRHGrDcMBbDLZHV
+wdvwmhghhbtBMcLtwZ
+gvQvvghTfPSmpmQljGFRjQbFGCsJbC
+BmphBWmDBBQfpVgQZpjg
+rqqGrrrqlnqqHqjNnVDSSSgQQffj
+FqLrbsLFsbLbLqHlMrmwwPDcCmMMmJBwDJcC
+wZccfslqZPFFjrFbFfQQ
+vvTvVWCJJZVBWCSvnVJJrhjQVMjVjzbgMQbjpphh
+BBmNWBvZRvSqwsGwssPcmc
+LGpnfcnzfzQdNFNHqHJptq
+RBNNvZSBRbRCCDJqHrDZqHFZtw
+SRNhRsNhWSNWsRRvgjngQnnMTnTgQjGMff
+twtZmwqBHtmqnnmlGLfcfvQQ
+dgMSPSMdQGclRRdF
+pVMrDgThDDlPWPWbBZtZqqttBqjqjT
+PPSWCGSzpCCQwNsNPFhTNVbB
+vqcgJngqLLcZLvBhNTVlbsvdFF
+DDmHgRjHZhHtGfWpQH
+sBLbwWWBvsBsqLqStRjcGGRnggjGcntJDn
+NCMQPMQPMQNzGGRRgRJRGzcG
+dHQNfPCFTQfFfVVNvwwJvSWSqWqrSqdS
+prDBnnDpFDprnDPBDQBvpBZttcSqSZSZcScFJSHcZJtz
+VhMVdLsjdqVWJSWZZZcHst
+hLVdGLfqjGjlfhCfCLjTTmrlpQBPBmvnrgDgpp
+SSSTJmmgbGwtmRZHCCZRCH
+FWcPQrrWqflzSWpRHZCZHRSt
+PSQzzdcQTghdndDJ
+cLlrNPvljRhRgTlM
+VmDBGnVdmJDnDBndnnVwDRvMgRsTbTzMMsgZghzzsB
+SpHvmGnSDJnwvDQqfrCPLpPLCfpF
+PppbRCCgpzzQCgCSgZTlNNTWnNNDNlRnGl
+hwMhLtBcBdjjNzWzlclcNGTs
+FjJFHJwhfwLHMLJLwPmqfQPgVCmQgCmCzC
+jTtMqFjMBqBmTntTztBTnTZBRZRLpLJgDgJNhghJrNNhhLJh
+DCDdvdGsVDVsflVdQSsfSwhRJlgppgpRpbWLgJbJpgbR
+wfSHSsdSVvQSwfwQPQPqHtFDcMzmtjBntqMTqn
+HQhQWLCSHCSCjnjQdSJdCSQgTTmZPTVZmqnTpPtnpmRmpp
+vDrzhvGzfchvlGvMFMrqVqPgZVVtZtVRgZgm
+bcvMfwvDsDfbvfwDbbdhCShWCBLLJWjHsHjj
+zLSsJNCjsjLCNLCgGcwBPPdwBwqwqz
+WMFZprZDbrddWRVRRDvlPPBcPhhlBqqHPGPhhffg
+VdbFvZWWWZZDFTLtCmNntjTnLSnn
+JLVhhwRbhVwcLFJFhhJcccqwsvpRlllvpWvZSBSSSRsNWpzl
+jgzfPffgZNspgZQS
+fjCmDCGnfmTfFqrFnhqbFzqt
+SfMRRNHSNNLfRfHcRRsqwdCCsssTqBCvgBLv
+llDDWQnFGtQnmtGQDWQFsgdHVBddndHsgqTsCTsg
+GzGjpWmWbmQmbpGGmGjHSrZcMfZSRPJPfcMRcb
+PDdMdRTRrLDSwzJvfSvJ
+FnjQnsqsFTnStvplhhzzFS
+TBHHCsgVRRcMHbLR
+GcLdGBJvBvLJHccJBvqHpGzDFfzwfzjwhDwrSFpfpDSn
+mZZrTTQVmQmlsMPVblZQVZmfCwjzzjChzCCbDSzhFjfnSb
+gZlRlZNPlmlgTTPmNRvJWcqrNLdvHWLBcHtH
+jWWbBwgwWwwtvvSCtHvgWsMFmscHzTGMmcssGFTTGz
+ZrLtpLnlfQJqnfJtpLnZlrqdNNGqcDNNFFTNDzzMMTMsMNMs
+LJQrnZnfLZnlrZflJJRVRQbbwBgCtCVjWgjBjjgbPjBB
+fqQVfRqSqmpnlLnm
+jFcjMJTjhwwggjFtgzCHmCzCmGzGlzpn
+wstMFFjWDfQRvmDPSB
+PgTFGPgcBZPcHPFBZRjGPgwCnmwCsmSdQdThmMMMQCQS
+JbpvWtvfHblWDHJDzmndSdMQnSwCdhMdQD
+rfvWlLlbtfJvvLJpqWbbqZRRGPVFNHVFgZNVFBgH
+TRMrrGBLMLPtbssTGtBHwZmdQQbdNzzZZNZZdwjd
+CVlVhCnclvhWSFFfQrWNrjmpNfwmjZ
+rlqFlclChhCvnlDvgVvRRtPtqTGJHRMBRTPPqM
+gZzCrQGQdrQvZHPTHWDbTgWPJM
+nSpLlcnnVjsSVLLnLSnhLSsJPTTWFsqfbPMFMqJDbfqM
+VwwnpwLnlPdQCwPPCC
+lRlrnlrsrMlhVsRnVhGPvCFNcPBDBvccrCGr
+RZQTzWTRdDNvBDdNcC
+TqjZbWRHmlMJgnmsng
+scQmLfQBQQvvZfLsmmvDJwpgSNSDDdcJSSwTGD
+PHlMbtzCCnlbztMRzlPNNdNwGpDpwgwptNLGpw
+rRHnLbhCzbbCHnHjMbzzjzFZmfQqWZQqvmhmfVZmqFBW
+fLTQWTMQtjcCGCJCbf
+gGsmsVSzmjCFHJCJgg
+SPRsSwSvBsPRPsqzwSVqzmhVWtLWhTDNLlTDtLTWGpNMtDLt
+mbzRbchRRQzzssLdhLggLddJ
+DCqDNNNWvDvjcPLsJcLLdv
+cpFCVNnVBHtbfFRtMRFf
+PFRcCCPtsDDDtjVspgwmgTNpTgTpspsw
+BqqqdJdHdMgSfMmZpZND
+vDHJGdLbLzBJdGnDdrBqVtCzWPPhthtPFzzPCFtV
+HvhvHdFdvJDfHdZdpfhrmGPljPRrGPPVDGrWWC
+NMMsRBMzcRRMMBSzcnbmNGrCVCWrCqPClmPqlG
+zLBbwMzQnRSQMThtZFLvpdgHtJfF
+DpcJcJPmMcLSHHZCfpnH
+BsBFvvqTFlbhgdbBBblfZLCLzfHWfjnjLCnCrh
+dTsNgqFvNgsGlZJRtVtMPmtDmG
+LdGQqzPGCCjJTJdTLJQJtFcFRSctcrFNFltPFtcc
+HphMMbbMdBMHbBhhgHMnhvwFFvtrlSNRNgcRllcvcc
+spHMhBnHnnsmWdnsnMBMdVGGmzjzLmZLDQCCCCZjqjTD
+DDZMzcTRgDMLzqCffhfWfcWnfj
+NsHVVJmswwSSwNPPNjnhqhnCCnhNvjfTnv
+rSSddrGSGrlMrpTpQT
+bbbfCfrLHMMMWVWC
+SqsvNZqQvvqcjNvqZsMMwgFgFplTHQVRFgWH
+BSZWWqBZBjmPGJGLbBtf
+RNCNfzfRHmzHwSdRdGfzRJPqFcFcDFGccZZqtLtGLtgl
+pjhVMhvhbjvPcDJvcZqt
+bsQMTsjppmSdTnHSJH
+PtLwpSwdSJwQnGvvqtvMhZ
+TlFcHlTjVjsDTQnCQhbZGCVVnb
+cjljTslTrlzzHDNRfNgLSNBJfBwNfG
+HvsZZqqqwWZswWHTmHsvvfhSfBfDffjchfBbhD
+MCpnCVpQClRNnlNQVQClfDhScmjBfLhmLDGbBNbS
+gmtpJpQQllJnWdZWwJWFwJJT
+TzBvBwwdhgRPGHlRHh
+NLWttJsrLWttppLpsGlsmVbVGRljGDRgjV
+MlllnNFnnQqCdzqq
+vptzrJhMMGGMptJPhJGJPvdFTFcSsTBVsczBScTSFFfn
+gbRjWgRjCqjZnfHCHnTSVBHF
+mRwqNbmqlbbjqRNlLbNrDJntNDGtDhNpGMrpvJ
+GPWZLgWqLHHGbgbbGPPmqHqfcjjRHJJBDRBRjBBjMHjwvwQc
+dhpFSpzVSSMSlDBvMQ
+TndsVNztVTspnsdpshtmZLPCGGNPfgqbWWfDGb
+CbqDjjCdClqgrfJvrv
+NGNPtGGzzHztPWWnlgJvfBnWBFgp
+hGzNHhsmGccwHPHZHcwdCCdbTVRTvSmTCjbLCb
+sqnqsHGpJbqnrbshpshHmmmCWZZmWwfTjTjHmfLZ
+dPggRgSDDttMFgctgdDtDcDcRWJBmjTWwmRLBLfmwBBjZWTB
+PFcVPlJPglbqhhrnnlNz
+wZdDNDdPPfhqwWqbsF
+VTngRzpnzMLvzTCLlhvDfltqqDttqFqs
+mggTVpCDDSNjBmPZrd
+cSdqJSTTTJcSJpCdQbqTCPPdjdDtGzwzjDwjwwwwzD
+rVvsBBVgsVBhHhfljtgbPgGtWjPtwt
+fvHsVZHVnRHpSJJRmbbSLT
+lDDPRRjwLGlvVRDRPlwwwPvmpSfhVWSzhqfzqpHpVpVHfqSH
+qBnqBNsBBChhCSfZ
+nTQbQnNNQJTLvlmTPLqqmG
+TTCJhDrmDpRVhvhHfffwzwfz
+dmmdmglWcqvHvWsHzB
+gdQMZbtlgQlZcMSttCNmVVrLSTTJ
+PLZLqhZZzZLBjjjGrrPjMH
+CcQcCcfRlWDjdrMrBrHC
+WlWFMcFpcRFmsWFcmflqSJzTqzwLvshggsZJwz
+LgqRDDDHHGTpgpJrQrQhhhCqrwPw
+ZSBWjjFshCFlQDrJ
+SjWnnbWtWnsztgGDDbDTGgHHGp
+llfvMlvzjzGzGRfvMSGRfSdStrCtQNCZrrFdJJLnNtLZ
+shhhshPHsTTqsBHTVTwTwZZnCtQrQnJtQCCJBCCZdZ
+TPHTPTHmDnljplfpGfGm
+qcNTmvvSvTNrWhRrTdthzW
+bDVJphpMMJwJpMHtrrttWsgwtzRW
+bFpGJbllPfplVQmnhvvcSmCFqq
+GTPJGMQTPQMqZjHTBmnndBVddHrrzNrz
+bbcRFgDpptRbffwmzmrvLmcZvmLmLv
+WCwgWbpgwtgfpfMlQGPhSPZWTZPl
+DsPCswsMPBMwPDCVJPnTPPWFGJNJmbJW
+RvvddfvftdtvNzghGSbFnWTntJSttGbG
+LNgRddgRlgzcgCDjjjHjcBCwcM
+gnVtgBnpwBgShBgcwhJJhjCMMMDmLRjDRMjrDMMMDMqMRF
+slsblHPNHlbTNbsPvszHQWbzqrZLMRmFMFmdFmrtDFdMLNZD
+bPzfvbfsvvlHtlzPHllHGTlTppCJpgcngcpwnwCGGJnnShwV
+cgQRgtzDbHPcgHzQWpTjTLdjjNNpNLsDss
+nwccZBmwcJqmJnjsTvmlTSsdlTNs
+CCGFCBVrBwwGBhqVnZBrqWMMQzHfQcHzzzPtfztGfg
+lhnwnhlbgbngbcfDgJLJQqDdVd
+FSrvtMFZVJJJVtcq
+jZNNNNjmjSPjFTJmGGzswwzHwHpBsbPblhhW
+tnDWHntzDtzQBZLMLzNLDDcRFFjhJBmcFRCTjRchcRvT
+sqwsPlbGfSbPGSVbJfpjjhcTFmCRjjvmTTvRdw
+lJqSqPVbgGSGrVSqJqflbWZQNDMLHnrQQWNDMtQMQz
+lpltwwJqsWVLPtVt
+DGHsDdZQzHLSLZcFRrFS
+BGsGCnHmMlMwCfwT
+nrRNzRMPrrPnNwNzTSFSTNtqZdtMttvQqQmjdjvZpgjZ
+GWVhGcGhHhGcffbZGDmmtttQvpdtbpppdj
+HGlHBhHGJfJJhCfZzLTTNnNrTnCNwT
+jBpCZStjBwWrQCMrhw
+TvcHBzHdPPzdvFTzzJlvzdQfThrhhrhfQTWMQfWMqfwf
+bzGJJBJcJvdvBPFzddGgjZSbZZngRZNNnnsjRs
+dqPqbpPFJfsFfMcNQNNtNmzrNQJn
+VVBDWvwZWDLwGlDhLGWWVcmQtSNmLmtSdSSTmrcQQm
+lZlhwDCdhhHllvWvjMHbgMpgffMggpPb
+SWSFLLFWDSWDNFzmmLMfGlfsdfnJMBfwMGVnBf
+vPtgZcctcTQQZRRcgCtZwRfqBVGqnVTBGVnqlsdBJnqV
+RcjgwbbgNSbFbhDb
+JrRZLrHvjQFPLnnBPQ
+DhwbtHbzpcpFTgtQ
+zlDwlHlzWSwDqhMMbSJVZVvrCrCZJZNZdJ
+fgNCZSDtDfDZTrTfqWghQGzGQshgpGGFQg
+RFvLnvFjnVjmLQGPQWLmWh
+MdwwVMFbMdRHFbccbCZJtbDJrqqJZNJZCZ
+fdZVBMMdfdfBCzhTzMdMCgCrGGrpQJmSmGJGmpJQVpLmqV
+RbFnhNsvlDsFHttllGmqGpPLvJpmPJSqLL
+tjNsDnNwbNjttNNZTzhMWzcZcMTwMd
+DjSSMShjRjPCbDFCdCSDbpBBswfNWZBZZrBVBPNfVmVf
+zltLjLqqGlzQntqqGztqcgncZrVrmNfNwWBVrVmrgwrfswsW
+qcTqHLlnJzGznLJtHGhMbhjFhMMpFbpbThpp
+ZVFZcctFQzsCtbZFnPPHqmqpwmvPmp
+NrjGfMgcLLcfdLqpmRwRRqJJmdPw
+LMNDgMBGlgGDLMNDGljctVbVWZTTCWChhTttsl
+RMGRRhhgzgZMtHdGTtvDwDJFCDvvwdvwqFFv
+rfrrjLNmmSnSjVSmNNPPbJVbqvqsqvhvFqCq
+flrpnSlrSNfjrNjSphNSWlHRGzTgtQGHQtttBTRBRQHW
+tplDDprhbvprvrJDprCpbsvHRfzSzTtzmRqSTznRRBRnSfFF
+MVwWjVNVQGfcMnTmRnBm
+VQwGLNLjWNWPGjZbsDBppBZhhDvBlZ
+RVVrGVVchRZsnzRzBWZb
+FQHWWCHwQmWmlqfCHSwJnsbNJnNsvttntBtb
+QQSFgqgqLMLPPdWdMVhWDT
+cZrMjncTdfJpPJbr
+WHNqnQwwCwvlqHtCtHNslNlvLLPDfSVdVPVDVSfVSbftffVf
+NCwwwQwllwnvgsvZzgzFZzBzjGGGMM
+MvHpfzcTcZzpphhbsDSTStsltqSDtS
+PRmnwCrWnWQrmNMRNnlNGbqlbDltdlbDtNtD
+CRJnmRVWJfgMLvcz
+HPFbHrrwLdVdgbDZqcphCqSZBhLZ
+tQRfRRGtvTNNSGTMjjmDCRhmqpBChqhsRDZh
+vjSfMzGQNQQnMtNTTWNNjgblFdFHwgdJJHHPwddgnr
+BggPRVBPPgfCBmJTjTTqpTNpZBwMbr
+lclLLllsQLFlsbMqNrMwTpwpcM
+SvbDzSDbWFJfWPPgdnfR
+GbpSSbGDNbSSJbDZNZbDppGtMntHLHvHCTLCJMHnRCMLTT
+cdwddjBfPsmPPQqQqscnHgRMtngvtjjgCCTMzM
+WvwPvWvflBwdQPlNVVhbDGpFhNGhbl
+WZRGmRvpCRFTZMQQQMCdddDDcD
+lqgqsgvjVtbMDzzbtcDQ
+NsNNgjNNjsNhnSvRmnpGRmSTSG
+nTgFtDTDDLrFBStdGdcHcbvGSc
+QPzfPCMzWCjfMPJhWGlRbRWRWrRRdVVH
+zhCrCQCjPrpNNBsNspNnwq
+zTJpqFzbTzsWsVbbfLGfSSCDNSBCHfMLHG
+rZcvtmhctrvmlPPmmmrhhmBLCHDCCLLDlqMlGMNDMwDC
+rRhRhnnQPZhtZcZtdttZgqFWWVjssqQpppWpFpJW
+NWPhdWJPWVzVqQrqmSsPbrPP
+cZDRjGsffGsCDfffgjGgRQSrTcTmSlTrbnqmSSrlln
+fFGCjGCjLDLFRgfDHZvzLVWtvsWWBtzJNWMB
+qMVbtnmMMTpCppsR
+NffHGrWzWWgDBfTRhChCnSCWcnjT
+QrlQBPBrlPHrrQlrHFLqPnLvVvbVmVVJtq
+MVMpHMZLVCpMrfWjvWnfrJ
+hlblzDDzwlSlGtRhRlSdrfGGWnWWfFPjJjnfqWGF
+lmlhBRlDhhhDRRhwDmBpHJmsNCHmCgNHJCJLsc
+jvsLgmqLgHvbPPVbNjSCjC
+pwTcRpRWLRMLJJFwBBGWcFWNVlDDCSTVttNPblZZCVVDlP
+GcdhccpcpRpGRhGmfsHHzLQQHrmsnh
+FMmSRgtMltMnVgnmNvlrsJrsZWjspvsZJp
+QbdhqwqbNqdHbTdcbcpsrpvjfWfLJLfwJrWp
+DQBBQqQGccdTPGqqBNtFGRSMRSFGtnVSnnmM
+fPjGrfFrrprprdrbQPZwlcZwZmlJwH
+qvNnvWnvWDvSvqNtWSLWStqbcJBQwQJwQZHLBZbcmJbblb
+DMtvqSvvDtntCRfwzGCgdzzFjG
+TfdZgtmfDgqgvlLFFsFHvcvZ
+pphWQMVjQVVBWWjRlHlHnlcLDDhcnF
+JQwwWVPBwMJpJwpWwGBWNzrDzSSzfgTPqTSTTtSPgt`
+
+const bagsArray = input.split('\n');
+
+const part1 = (bagsArray) => {
+    let sumPriorities = 0;
+
+    bagsArray.forEach(bag => {
+        const itemsArray = bag.split('');
+        const middleIndex = Math.ceil(itemsArray.length / 2);
+        const firstCompartment = itemsArray.splice(0, middleIndex);
+        const secondCompartment = itemsArray.splice(-middleIndex)
+        let commonCharacter;
+
+        for (let i in firstCompartment) {
+            if (secondCompartment.includes(firstCompartment[i])) { commonCharacter = firstCompartment[i]; }
+        }
+
+        if (commonCharacter.charCodeAt(0) >= 'a'.charCodeAt(0)) {
+            console.log(commonCharacter, commonCharacter.charCodeAt(0))
+            sumPriorities += commonCharacter.charCodeAt(0) - 96
+        } else {
+            console.log(commonCharacter, commonCharacter.charCodeAt(0))
+            sumPriorities += commonCharacter.charCodeAt(0) - 38
+        }
+    });
+
+    console.log(sumPriorities)
+}
+part1(bagsArray);
+
+const part2 = (bagsArray) => {
+    let groupsArray = []
+    let bagsBuffer = []
+    let sumPriorities = 0;
+
+    for (let i = 0; i <= bagsArray.length; i++) {
+        if (i % 3 === 0 && i != 0) {
+            groupsArray.push(bagsBuffer)
+            bagsBuffer = []
+            bagsBuffer.push(bagsArray[i])
+        } else {
+            bagsBuffer.push(bagsArray[i])
+        }
+    }
+
+    groupsArray.forEach(group => {
+        let commonCharacter;
+        // let lengths = []
+        
+        // let maxLength= Math.max.apply(Math, lengths);
+
+        group.sort((a,b) => b.length - a.length) // Trier de la string la plus longue à la plus courte pour passer par tous les caractères lors de l'étape suivante
+
+        for (let char in group[0]) {
+            if (group[1].includes(group[0][char])) {
+                if (group[2].includes(group[0][char])) {
+                    commonCharacter = group[0][char]
+                }
+            }
+        }
+
+        if (commonCharacter.charCodeAt(0) >= 'a'.charCodeAt(0)) {
+            sumPriorities += commonCharacter.charCodeAt(0) - 96
+        } else {
+            sumPriorities += commonCharacter.charCodeAt(0) - 38
+        }
+    })
+
+    console.log(sumPriorities)
+}
+part2(bagsArray);
